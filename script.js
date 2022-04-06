62be618ddb2f350eacb9ffdedddb18a62e7debf0
Yomi-Dev <yomi21e@gmail.com>
2022-04-06 15:22:30 +0100

    2nd commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+let apiQuotes = [];
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// Show New Quote
+function newQuote() {
+    loading();
+    // Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+// Check if Author field is blank and replace with 'Unkown'
+if (!quote.author) {
+    authorText.textContent = 'Unknown';
+} else {
+    authorText.textContent = quote.author;
+}
+// Check Quot length to determine styling
+if (quote.text.length > 2) {
+    quoteText.classList.add('long-quote');
+} else {
+    quoteText.classList.remove('long-quote');
+}
+
+// Set Quote, Hide Loader
+quoteText.textContent = quote.text;
+complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+    } catch (error) {
+        // Catch Error Here
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl =`https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listener
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// On Load
+getQuotes();
